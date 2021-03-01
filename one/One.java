c4312b1626acfffeea45b09130fa459cb13d460e
zking63 <zoemking63@gmail.com>
2021-02-28 20:36:57 -0500

    One2

--- one/One.java
+++ one/One.java
@@ -1,17 +1,30 @@
 import java.util.ArrayList;
 
 public class One {
-    public ArrayList<String> One(Integer Digits){
+    public void One(Integer Digits){
         //create a formula that gets a number down to single digits by adding each digit
         String temp = Integer.toString(Digits);
-        ArrayList<String> numbers = new ArrayList<String>();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        ArrayList<Integer> numbersFinal = new ArrayList<Integer>();
         int sum = 0;
         for(int i = 0; i < temp.length(); i++){
             String singleDigit = String.valueOf(temp.charAt(i));
-            numbers.add(singleDigit);
+            Integer digit = Integer.parseInt(singleDigit); 
+            numbers.add(digit);
             System.out.println(numbers);
+            System.out.println(numbers.size());
+            for(int j = 0; j < numbers.size(); j++){
+                String tempsum = Integer.toString(sum);
+                String sumDigit = String.valueOf(tempsum.charAt(j));
+                Integer sumdigit = Integer.parseInt(sumDigit); 
+                numbersFinal.add(sumdigit);
+                while (numbersFinal.size() > 1){
+                    sum += numbers.get(i);
+                }
+            }
         }
-        for(int j = 0; j < numbers.size(); j++){
+        System.out.println(sum);
+        /*for(int j = 0; j < numbers.size(); j++){
             //int digit = (int) numbers.indexOf(j); 
             //int digit = (int) Integer.valueOf(numbers.indexOf(j));
             //System.out.println(digit);
@@ -21,6 +34,5 @@ public class One {
         /*for(int j = 0; j < numbers.size(); j++){
             sum += numbers.indexOf(j); 
         }*/
-        return numbers;
     }
 }
