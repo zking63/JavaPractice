49341e80e6a70f6af77cd5ae9c0af5a9cabcea7c
zking63 <zoemking63@gmail.com>
2021-03-05 21:31:40 -0500

    One.March5

--- one/One.java
+++ one/One.java
@@ -1,38 +1,32 @@
 import java.util.ArrayList;
 
 public class One {
+    private ArrayList<Integer> numbers;
+
     public void One(Integer Digits){
         //create a formula that gets a number down to single digits by adding each digit
         String temp = Integer.toString(Digits);
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
-        ArrayList<Integer> numbersFinal = new ArrayList<Integer>();
-        int sum = 0;
+        numbers = new ArrayList<Integer>();
+        Integer sum = 0;
         for(int i = 0; i < temp.length(); i++){
             String singleDigit = String.valueOf(temp.charAt(i));
             Integer digit = Integer.parseInt(singleDigit); 
             numbers.add(digit);
             System.out.println(numbers);
-            System.out.println(numbers.size());
-            for(int j = 0; j < numbers.size(); j++){
-                String tempsum = Integer.toString(sum);
-                String sumDigit = String.valueOf(tempsum.charAt(j));
-                Integer sumdigit = Integer.parseInt(sumDigit); 
-                numbersFinal.add(sumdigit);
-                while (numbersFinal.size() > 1){
-                    sum += numbers.get(i);
+            sum += numbers.get(i);
+        }
+            while (numbers.size() > 1){
+                numbers.clear();
+                numbers.add(sum);
+                String temp2 = Integer.toString(sum);
+                for(int j = 0; j < temp2.length(); j++){
+                    String singleDigit2 = String.valueOf(temp2.charAt(j));
+                    Integer digit2 = Integer.parseInt(singleDigit2);
+                    numbers.clear();
+                    numbers.add(digit2);
+                    //add in the current sum and then delete everything before
                 }
             }
-        }
         System.out.println(sum);
-        /*for(int j = 0; j < numbers.size(); j++){
-            //int digit = (int) numbers.indexOf(j); 
-            //int digit = (int) Integer.valueOf(numbers.indexOf(j));
-            //System.out.println(digit);
-            String digit = String.valueOf(numbers.indexOf(j));
-            System.out.println(digit);
-        }
-        /*for(int j = 0; j < numbers.size(); j++){
-            sum += numbers.indexOf(j); 
-        }*/
     }
 }
