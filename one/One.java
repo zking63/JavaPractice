cae769e009bc5cf2700f3920fa043c51dd2a0456
zking63 <zoemking63@gmail.com>
2021-03-07 13:20:01 -0500

    OneFinal

--- one/One.java
+++ one/One.java
@@ -12,25 +12,19 @@ public class One {
             String singleDigit = String.valueOf(temp.charAt(i));
             Integer digit = Integer.parseInt(singleDigit); 
             numbers.add(digit);
-            System.out.println(numbers);
             sum += numbers.get(i);
-            System.out.println(sum);
         }
-            while (numbers.size() > 1){
+        while (sum > 9){
+            String temp2 = Integer.toString(sum);
+            sum = 0;
+            for(int j = 0; j < temp2.length(); j++){
                 numbers.clear();
-                System.out.println(numbers);
-                String temp2 = Integer.toString(sum);
-                for(int j = 0; j < temp2.length(); j++){
-                    String singleDigit2 = String.valueOf(temp2.charAt(j));
-                    System.out.println(singleDigit2);
-                    Integer digit2 = Integer.parseInt(singleDigit2);
-                    System.out.println(digit2);
-                    numbers.add(digit2);
-                    System.out.println(numbers);
-                    sum += numbers.get(j);
-                    System.out.println(sum);
-                    //add in the current sum and then delete everything before
-                }
+                String singleDigit2 = String.valueOf(temp2.charAt(j));
+                Integer digit2 = Integer.parseInt(singleDigit2);
+                numbers.add(digit2);
+                sum += numbers.get(0);
             }
+        }
+        System.out.println(sum);
     }
 }
--- one/OneTest.java
+++ one/OneTest.java
@@ -3,5 +3,7 @@ public class OneTest {
         One result = new One();
 
         result.One(555);
+        result.One(100);
+        result.One(77777);
     }
 }
