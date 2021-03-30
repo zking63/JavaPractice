4a0a14b18c37e932eca4de98809afb5e68ad59e7
zking63 <zoemking63@gmail.com>
2021-03-30 08:58:11 -0400

    math in

--- fibonacci/fibonacci.java
+++ fibonacci/fibonacci.java
@@ -3,8 +3,18 @@ public class fibonacci{
     public Integer fibonacci(int[] numbers){
         ArrayList<Integer> fibsequence = new ArrayList<Integer>();
         for (int i = 0; i < numbers.length; i++){
-            int j = numbers[i];
-            System.out.println(j);
+            if(i==0){
+                int j = 0;
+                System.out.println(j);
+            }
+            if (i==1){
+                int j = numbers[i -1];
+                System.out.println(j);
+            }
+            if(i>=2){
+                int j = numbers[i -1] + numbers[i -2];
+                System.out.println(j);
+            }
         }
         return 2;
     }
