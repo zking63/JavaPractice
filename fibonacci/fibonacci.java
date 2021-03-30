31cb158e54ed32a58218270fe8d205cd454d05f0
zking63 <zoemking63@gmail.com>
2021-03-30 09:01:16 -0400

    arraylist added

--- fibonacci/fibonacci.java
+++ fibonacci/fibonacci.java
@@ -1,21 +1,22 @@
 import java.util.ArrayList;
 public class fibonacci{
-    public Integer fibonacci(int[] numbers){
+    public ArrayList<Integer> fibonacci(int[] numbers){
         ArrayList<Integer> fibsequence = new ArrayList<Integer>();
         for (int i = 0; i < numbers.length; i++){
             if(i==0){
                 int j = 0;
-                System.out.println(j);
+                fibsequence.add(j);
             }
             if (i==1){
                 int j = numbers[i -1];
-                System.out.println(j);
+                fibsequence.add(j);
             }
             if(i>=2){
                 int j = numbers[i -1] + numbers[i -2];
-                System.out.println(j);
+                fibsequence.add(j);
             }
         }
-        return 2;
+        System.out.println(fibsequence);
+        return fibsequence;
     }
 }
