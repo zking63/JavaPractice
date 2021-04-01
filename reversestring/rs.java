6fd6f179c6fc803b3d9a99416cbba5016309ade8
zking63 <zoemking63@gmail.com>
2021-04-01 19:08:09 -0400

    final

--- reversestring/rs.java
+++ reversestring/rs.java
@@ -1,15 +1,17 @@
+//reverse a string and print an arraylist of it
 import java.util.ArrayList;
 public class rs{
     private ArrayList<String> back;
     public void reverse(String word){
-        System.out.println(word);
         back = new ArrayList<String>();
         String lastLetter = String.valueOf(word.charAt(word.length()-1));
-        System.out.println(lastLetter);
+        back.add(lastLetter);
         String firstLetter = String.valueOf(word.charAt(0));
-        back.add(firstLetter);
-        for (int i = 0; i < word.length(); i++){
-            System.out.println(firstLetter);
+        for (int i = word.length() -2; i > 0; i--){
+            String letter = String.valueOf(word.charAt(i));
+            back.add(letter);
         }
+        back.add(firstLetter);
+        System.out.println(back);
     }
 }
--- reversestring/rstest.java
+++ reversestring/rstest.java
@@ -1,7 +1,10 @@
+//reverse a string and print an arraylist of it
 public class rstest{
     public static void main(String[] args){
         rs result = new rs();
 
         result.reverse("hello");
+        result.reverse("Zoe!");
+        result.reverse("HOW ABOUT THIS");
     }
 }
