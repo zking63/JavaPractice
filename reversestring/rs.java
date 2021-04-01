77e5fcac2fea79988b4f0795c718cd048f85dd19
zking63 <zoemking63@gmail.com>
2021-04-01 18:54:36 -0400

    last letter added

--- reversestring/rs.java
+++ reversestring/rs.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
 public class rs{
+    private ArrayList<String> back;
     public void reverse(String word){
         System.out.println(word);
+        back = new ArrayList<String>();
+        String letter = String.valueOf(word.charAt(word.length()-1));
+        System.out.println(letter);
+        for (int i = 0; i < word.length(); i++){
+        }
     }
 }
