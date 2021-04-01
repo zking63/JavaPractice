1c9a2818596886b43d2e799a0823c323196f4a9d
zking63 <zoemking63@gmail.com>
2021-03-31 20:38:40 -0400

    exponent added

--- lastDigExponent/last.java
+++ lastDigExponent/last.java
@@ -1,8 +1,11 @@
+import java.lang.Math;
 public class last{
     public static void lastD(Integer digit, Integer exponent){
         String temp = Integer.toString(digit);
         String last = String.valueOf(temp.charAt(temp.length()-1));
         Integer final1 = Integer.parseInt(last);
         System.out.println(final1);
+        Double result = Math.pow(final1, exponent);
+        System.out.println(result);
     }
 }
