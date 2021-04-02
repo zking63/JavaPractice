0d60cc2ca83fc3a98a6431156720c8f3a9a8fa1b
zking63 <zoemking63@gmail.com>
2021-04-02 14:20:17 -0400

    added replace function

--- removeBlanks/rb.java
+++ removeBlanks/rb.java
@@ -1,5 +1,14 @@
+//remove the blanks from a string of multiple words
+import java.lang.String;
 public class rb{
-    public void rb(){
-        
+    public void trim(String sent){
+        String newS = sent.trim();
+        String leftRemoved = "";
+        System.out.println(newS + ".");
+        for (int i = 0; i < newS.length(); i++){
+            System.out.println(newS.charAt(i));
+            leftRemoved = newS.replaceAll("\\s+", "");
+        }
+        System.out.println(leftRemoved);
     }
 }
--- removeBlanks/rbtest.java
+++ removeBlanks/rbtest.java
@@ -1,6 +1,7 @@
 //remove the blanks from a string of multiple words
 public class rbtest{
     public static void main(String[] args){
-
+        rb result = new rb();
+        result.trim("ok now ");
     }
 }
