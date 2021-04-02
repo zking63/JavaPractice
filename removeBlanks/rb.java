ee6f273371da232c9777be23ad4ef416700dac95
zking63 <zoemking63@gmail.com>
2021-04-02 13:53:23 -0400

    classes created

---
+++ removeBlanks/rb.java
@@ -0,0 +1,5 @@
+public class rb{
+    public void rb(){
+        
+    }
+}
---
+++ removeBlanks/rbtest.java
@@ -0,0 +1,6 @@
+//remove the blanks from a string of multiple words
+public class rbtest{
+    public static void main(String[] args){
+
+    }
+}
